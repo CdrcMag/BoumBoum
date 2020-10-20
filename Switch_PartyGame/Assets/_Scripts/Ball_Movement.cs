dde820bce29eb5c28731ac3d53fb1bb91febf41f
CdrcMag <44393506+CdrcMag@users.noreply.github.com>
2020-10-20 13:54:52 +0200

    Initial commit

---
+++ Switch_PartyGame/Assets/_Scripts/Ball_Movement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Movement : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private float x;
+    private float z;
+
+    public float constantSpeed;
+    public float resetTime;
+
+    private bool canCollide = true;
+
+    private void Start()
+    {
+        x = Random.Range(-20, 20);
+        z = Random.Range(-20, 20);
+
+        rb.AddForce(new Vector3(x, 0, z), ForceMode.VelocityChange);
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+        rb.velocity = constantSpeed * (rb.velocity.normalized);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            if(canCollide)
+            {
+                StartCoroutine(resetHit());
+            }
+                
+        }
+
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Blocker"))
+        {
+            if (canCollide)
+            {
+                StartCoroutine(resetHit());
+            }
+        }
+    }
+
+    
+    IEnumerator resetHit()
+    {
+        canCollide = false;
+        yield return new WaitForSeconds(resetTime);
+        canCollide = true;
+    }
+
+}
---
+++ Switch_PartyGame/Assets/_Scripts/Player_Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player_Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+    private Vector3 movement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = 0;
+        movement.z = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+        if (movement.magnitude  > 0.1f)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.1f);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movement * speed;
+    }
+
+
+
+
+
+}
